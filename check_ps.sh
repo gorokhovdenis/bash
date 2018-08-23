58235dd7c66fd91c80f0791d5881c519e7502b36
Denis <gorokhov@gmx.com>
2018-08-23 20:58:20 +0300

    first commit

---
+++ check_ps.sh
@@ -0,0 +1,9 @@
+N=12
+a="$(ps -aux | grep bot_old| grep python3.6 | awk -v N=$N '{print $N}')"
+b="/home/python/bot_old.py"
+if [[ "$a" == "$b" ]]
+then
+	:
+else
+	tmux new -d -s coindesk 'python3.6 /home/python/bot_old.py'
+fi
